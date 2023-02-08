35e280ad8cf0d645b7c8ee206b723756c9e158c0
venkat <venkat.mobiqa@gmail.com>
2023-02-08 11:36:16 +0530

    updating ./learn-terraform

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -1,15 +1,19 @@
-#variable "sample" {
-#  default = 10
-#
-#}
-#
-#output "sample" {
-#  value = var.sample
-#}
+variable "sample" {
+  default = 10
+
+}
+
+output "sample" {
+  value = var.sample
+}
 
 # String Data type
 variable "sample1" {
   default = "Hello World"
+}
+
+output "sample1" {
+  value = var.sample1
 }
 
 # Number data type
@@ -17,7 +21,14 @@ variable "sample2" {
   default = 100
 }
 
+output "sample2" {
+  value = var.sample2
+}
 # Boolean Data type
 variable "sample3" {
   default = true
+}
+
+output "sample3" {
+  value = var.sample3
 }
