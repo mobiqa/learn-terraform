f6e369593254868a92a28e1351db39a9f7f540de
venkat <venkat.mobiqa@gmail.com>
2023-02-12 20:40:21 +0530

    updating ./learn-terraform

--- 02-datasources/data.tf
+++ 02-datasources/data.tf
@@ -2,8 +2,6 @@ data "aws_ami" "example" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
-
-
 }
 
 output "ami" {
@@ -18,10 +16,10 @@ output "imagelocation" {
 
 }
 
-#data "aws_instance" "foo" {
-#  instance_id = "i-0bd3c2ffe91e93669"
-#
-#  }
-#output "instance" {
-#  value = data.aws_instance.foo.public_ip
-#}
+data "aws_instance" "foo" {
+  instance_id = "i-0bd3c2ffe91e93669"
+
+  }
+output "instance" {
+  value = data.aws_instance.foo.public_ip
+}
