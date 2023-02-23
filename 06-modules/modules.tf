d61f09b68511120599cadf01288a95d88c0260d6
venkat <venkat.mobiqa@gmail.com>
2023-02-23 10:47:57 +0530

    updating ./learn-terraform

--- 06-modules/modules.tf
+++ 06-modules/modules.tf
@@ -10,7 +10,7 @@ module "ec2" {
 
 terraform {
   backend "s3" {
-    bucket = "mobiqa"
+    bucket = "terraform-ncrmobiqa"
     key    = "06-modules/terraform.tfstate"
     region = "us-east-1"
   }
