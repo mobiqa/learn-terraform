87d47204f542415129cfdc2b93a7a94260228e51
venkat <venkat.mobiqa@gmail.com>
2023-05-08 09:29:19 +0530

    updating ./learn-terraform

--- 02-datasources/data.tf
+++ 02-datasources/data.tf
@@ -8,19 +8,19 @@ output "ami" {
   value = data.aws_ami.example
 }
 
-#output "snapshotid" {
-#  value = data.aws_ami.example.root_snapshot_id
-#}
-#output "imagelocation" {
-#  value = data.aws_ami.example.image_location
-#
-#}
+output "snapshotid" {
+  value = data.aws_ami.example.root_snapshot_id
+}
+output "imagelocation" {
+  value = data.aws_ami.example.image_location
 
-#data "aws_instance" "foo" {
-#  instance_id = "i-066cbd32dd320b769"
-#
-#  }
-#output "instance" {
-#  value = data.aws_instance.foo
-#}
+}
+
+data "aws_instance" "foo" {
+  instance_id = "i-066cbd32dd320b769"
+
+  }
+output "instance" {
+  value = data.aws_instance.foo
+}
 
